fe3710d604bd1140ae90d6ab72fb8efe94b13f26
Dominic <baozhifang@come-future.com>
2023-06-10 17:16:20 +0800

    feat: 初始化脚手架

---
+++ src/page/main/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './main.less'
+
+// 使用DndProvider的一个注意点
+// DndProvider不可以和钩子在一个组件中
+const Main = () => {
+  return <div className="main">
+    hellow world
+  </div>
+}
+
+export default Main
